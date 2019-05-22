9311a6ea96f7603143d07bd5e9ca20bfa1a3738f
Gao Xu <394845369@qq.com>
2019-05-22 23:31:57 +0800

    web 页面组成之html

---
+++ demo/web_html/web页面组成之html.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+
+"""
+selenium代码库
+77,68,56,59
+版本匹配 chromedriver
+
+<></>
+元素，<起始标签，属性名=属性值>文本内容</结束标签名>
+
+元素共同属性，id唯一标识 在当前整个html
+    class 类别   给元素归类 有很多元素有同样的class值，不是唯一的
+    style name  内联样式设置 position
+    name
+javascript DOM 对象，如何进行交互
+
+"""
