841f2538ebfe8012e48ae1d7d51c1dc691c6fae1
Gao Xu <394845369@qq.com>
2019-06-19 21:36:36 +0800

    commit study 框架

---
+++ demo/项目实战框架/PO_V1/PageObjects/bid_page.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/6/13 0013 21:37 
+# @Author : 蓝天下的风 
+# @Site :  
+# @File : bid_page.py 
+# @Software: PyCharm
+
+class BidPage:
+
+    pass
